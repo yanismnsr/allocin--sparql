bf67a04355b6e7d77d73cbe5593c5ba780c43416
Yanis <mansouryanis6@gmail.com>
2022-01-10 17:00:37 +0100

    search bar alpha version

--- allocine-sparql/src/components/SearchBar/SearchBar.tsx
+++ allocine-sparql/src/components/SearchBar/SearchBar.tsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import styles from './SearchBar.module.css';
+import { ISearchBarState } from "./ISearchBarState";
+import { ISearchBarProps } from "./ISearchBarProps";
+import { textChangeRangeIsUnchanged } from 'typescript';
 
-// const SearchBar = () => (
-//   <div className={styles.SearchBar}>
-//     SearchBar Component
-//   </div>
-// );
+export default class SearchBar extends React.Component<ISearchBarProps, ISearchBarState> {
 
-export default class SearchBar extends React.Component {
+  constructor(props: ISearchBarProps) {
+    super(props);
+
+    // Setting the state
+    this.setState({
+      searchString: ''
+    });
+
+    // Binding handlers
+    this._handleChange = this._handleChange.bind(this);
+    this._handleSearch = this._handleSearch.bind(this);
+  }
+
+  private _handleChange (event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      searchString: event.target.value
+    })
+  }
+
+  private _handleSearch() {
+    // Build the sparql query
+    const { searchString } = this.state;
+    console.log(searchString);
+  }
+
   public render() {
     return (
       <div className={styles.SearchBar}>
-        <input type="text" id="filmName" />
-        <button type="submit">Search</button>
+        <input type="text" id="filmName" onChange={this._handleChange}/>
+        <button type="submit" onClick={this._handleSearch}>Search</button>
       </div>
     );
   }
 }
-
-// export default SearchBar;
