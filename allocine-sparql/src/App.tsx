e9a227379b721c5e78331f4e956157e199c09024
ReversedHourglass <tom.alegre@outlook.fr>
2022-01-23 18:38:40 +0100

    films cards cliquables

--- allocine-sparql/src/App.tsx
+++ allocine-sparql/src/App.tsx
@@ -1,53 +1,53 @@
-import React, {useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SearchBar from "./components/SearchBar/SearchBar"
-import Details from "./components/Details/Details";
-import { BrowserRouter, Route, Routes} from "react-router-dom";
-import Carousel from "./components/Carousel/Carousel";
-import {Movie} from "./models/types";
-import Home from './components/Home/Home';
-import {Service} from "./services/Service";
-import {Spinner} from "./components/Spinner/Spinner";
-
+import React, { useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import SearchBar from './components/SearchBar/SearchBar'
+import Details from './components/Details/Details'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Carousel from './components/Carousel/Carousel'
+import { Movie } from './models/types'
+import Home from './components/Home/Home'
+import { Service } from './services/Service'
+import { Spinner } from './components/Spinner/Spinner'
 
 function App() {
-
-    const [movies, setMovies] = React.useState<Movie[]>([]);
-    const [selectedMovie, setSelectedMovie] = React.useState<Movie>();
+    const [movies, setMovies] = React.useState<Movie[]>([])
+    const [selectedMovie, setSelectedMovie] = React.useState<Movie>()
 
     useEffect(() => {
-        console.log("getting movies from dbpedia");
-        const serviceInstance = Service.GetInstance();
-        serviceInstance.fetchMovie({}, {size:10, page:1}).then((result) => {
-            console.log(result.results.bindings);
+        console.log('getting movies from dbpedia')
+        const serviceInstance = Service.GetInstance()
+        serviceInstance.fetchMovie({}, { size: 10, page: 1 }).then((result) => {
+            console.log(result.results.bindings)
             const foundMovies = result.results.bindings.map((m: any) => {
                 return {
                     title: m.title.value,
-                    description: "test",
+                    description: 'test',
                     releaseYear: m.releaseYear?.value,
-                    urlThumbnail: m.urlThumbnail?.value
+                    urlThumbnail: m.urlThumbnail?.value,
                 }
             })
-            setMovies(foundMovies);
-        });
-        console.log(movies);
-
-     }, []);
+            setMovies(foundMovies)
+        })
+        console.log(movies)
+    }, [])
 
     return (
         <div className="App">
-            <SearchBar setMovies={setMovies}/>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Home movies={movies}/>} />
-                        <Route path="/details" element={<Details selectedMovie={selectedMovie}/>} />
-                    </Routes>
-                </BrowserRouter>
-            <Spinner/>
+            <SearchBar setMovies={setMovies} />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home movies={movies} />} />
+                    <Route
+                        path="/details"
+                        element={<Details selectedMovie={selectedMovie} />}
+                    />
+                </Routes>
+            </BrowserRouter>
+            <Spinner />
             {/* <Carousel movies={movies}/> */}
         </div>
-  );
+    )
 }
 
-export default App;
+export default App
--- allocine-sparql/src/components/FilmCard/FilmCard.tsx
+++ allocine-sparql/src/components/FilmCard/FilmCard.tsx
@@ -1,23 +1,29 @@
-import React from "react";
+import React from 'react'
 
-import styles from "./film-card.module.css";
-import Rate from "../Rate/Rate";
-import IFilmCardProps from "./IFilmCardProps";
+import styles from './film-card.module.css'
+import Rate from '../Rate/Rate'
+import IFilmCardProps from './IFilmCardProps'
+import { Link } from 'react-router-dom'
 
 export default function FilmCard(props: IFilmCardProps) {
+    return (
+        <Link to="/details">
+            <div className={styles.filmCard}>
+                <div className={styles.filmImageBox}>
+                    <img
+                        className={styles.filmImage}
+                        src={props.filmThumbnail}
+                        alt=""
+                    />
+                </div>
 
-    return(
-        <div className = {styles.filmCard}>
-            <div className = {styles.filmImageBox}>
-                <img className = {styles.filmImage} src={ props.filmThumbnail } alt=""/>
-            </div>
-
-            <div className = {styles.filmContent}>
-                <p className = {styles.filmTitle}>{props.filmTitle}</p>
-                <div className = {styles.row}>
-                    <p className = {styles.filmYear}>{props.filmYear}</p>
+                <div className={styles.filmContent}>
+                    <p className={styles.filmTitle}>{props.filmTitle}</p>
+                    <div className={styles.row}>
+                        <p className={styles.filmYear}>{props.filmYear}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
